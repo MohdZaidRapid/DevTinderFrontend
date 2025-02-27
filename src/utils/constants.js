17de29a7f2d1495cef88f2c92f0477fe5e23d607
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2025-02-28 03:30:34 +0530

    added prod url from render

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,9 +2,6 @@
 
 // dev
 export const BASE_URL =
-  location.hostname === "localhost" ? "http://localhost:7777" : "/api";
-
-
-
-  
- 
+  location.hostname === "localhost"
+    ? "http://localhost:7777"
+    : "https://backend-tinder-zfqy.onrender.com";
