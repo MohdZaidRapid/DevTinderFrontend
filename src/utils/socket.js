94b4c985227a49393f397d4584e3fd502ae2f871
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2025-02-28 17:00:21 +0530

    added

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -3,8 +3,24 @@ import { BASE_URL } from "./constants";
 
 export const createSocketConnection = () => {
   if (location.hostname === "localhost") {
-    return io(BASE_URL);
+    return io(BASE_URL, {
+      withCredentials: true, // Ensures cookies (if any) are sent
+    });
   } else {
-    return io("/", { path: "/api/socket.io" });
+    return io("https://backend-tinder-zfqy.onrender.com", {
+      path: "/socket.io",
+      withCredentials: true,
+    });
   }
 };
+
+// import io from "socket.io-client";
+// import { BASE_URL } from "./constants";
+
+// export const createSocketConnection = () => {
+//   if (location.hostname === "localhost") {
+//     return io(BASE_URL);
+//   } else {
+//     return io("/", { path: "/api/socket.io" });
+//   }
+// };
