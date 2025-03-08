29ca0f0a478e801ecd5013c8b4b18ab410cd3022
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2025-03-09 03:28:28 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Provider, useDispatch } from "react-redux";
+import { addUser } from "./utils/userSlice";
+import appStore from "./utils/appStore";
 import Body from "./components/Body";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
-import { Provider } from "react-redux";
-import appStore from "./utils/appStore";
 import Feed from "./components/Feed";
 import Connections from "./components/Connections";
 import Requests from "./components/Requests";
 import Premium from "./components/Premium";
 import Chat from "./components/Chat";
 import BlockedUser from "./components/BlockedUser";
-import WithRoleAuth from "./components/WithRoleAuth"; // Capitalized HOC
-import Example from "./components/Example"; // Capitalized Component
+import WithRoleAuth from "./components/WithRoleAuth";
+import Example from "./components/Example";
 import AdminDashboard from "./components/AdminDashboard";
 import CheckBlockStatus from "./components/CheckBlockStatus";
 import Blocked from "./components/Blocked";
 import VideoCall from "./components/VideoCall";
 import PostContainer from "./components/PostContainer";
 
+function SyncUserWithLocalStorage() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      dispatch(addUser(JSON.parse(storedUser))); // Restore user from localStorage
+    }
+  }, [dispatch]);
+
+  return null; // This component doesn't render anything
+}
+
 function App() {
   return (
     <Provider store={appStore}>
+      <SyncUserWithLocalStorage />
       <BrowserRouter basename="/">
         <CheckBlockStatus />
-
         <Routes>
           <Route path="/" element={<Body />}>
             <Route path="/" element={<Feed />} />
